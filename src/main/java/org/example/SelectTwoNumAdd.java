4fd50f53228866c27a8044c1c0eb72669c1183c0
machdd <jkk1637@naver.com>
2023-06-05 22:18:28 +0900

    no message

---
+++ src/main/java/org/example/SelectTwoNumAdd.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class SelectTwoNumAdd {
+    public int[] solution(int[] numbers) {
+        int[] answer;
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int i = 0 ; i < numbers.length; i++){
+            for(int j = i+1 ; j < numbers.length; j++){
+                int tmp = numbers[i] + numbers[j];
+                if(!list.contains(tmp)){
+                    list.add(tmp);
+                }
+            }
+        }
+
+        int size = 0;
+        answer = new int[list.size()];
+        for(int num : list){
+            answer[size++] = num;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
